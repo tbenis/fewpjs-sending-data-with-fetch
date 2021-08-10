332a37b0196349d3e0a15dea7e27e075b4f83395
Benis Tambe <will.jenifer@yahoo.com>
2021-08-10 00:28:01 -0400

    commit tree dec9b9ed958fcf1f352368485d13e99c9c448070

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+    const data = {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({name, email})
+    }
+
+    return fetch("http://localhost:3000/users", data)
+    .then(response => response.json())
+    .then( (obj) =>  {
+        document.body.innerHTML = obj[ "id" ];
+        // const div = document.createElement('div');
+        // div.appendChild(obj);
+        // documennt.querySelect("body").append(div);
+    })
+    .catch(function(err) {
+        document.body.innerHTML= err.message;
+    })
+}
